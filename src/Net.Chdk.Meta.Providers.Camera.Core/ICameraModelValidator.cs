d3c3614e22b4c724b93a1046cbe0b13e94bc80b8
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 05:51:10 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Camera.Core/ICameraModelValidator.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Meta.Model.CameraList;
+using Net.Chdk.Meta.Model.CameraTree;
+
+namespace Net.Chdk.Meta.Providers
+{
+    public interface ICameraModelValidator
+    {
+        void Validate(string platform, ListPlatformData list, TreePlatformData tree);
+    }
+}
